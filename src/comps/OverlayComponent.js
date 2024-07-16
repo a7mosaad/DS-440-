c137805794a19efb44010badcbc43e05346185cf
a7mosaad <a7mosaad@gmail.com>
2024-07-16 09:38:12 -0400

    python file update, new overlay method

--- src/comps/OverlayComponent.js
+++ src/comps/OverlayComponent.js
@@ -1,17 +1,36 @@
 import React, { useContext } from 'react';
-import { GeoJSON } from 'react-leaflet';
+import { GeoJSON, useMap } from 'react-leaflet';
 import { GeoDataContext } from '../GeoDataContext';
 
 const OverlayComponent = () => {
   const { geojsonData } = useContext(GeoDataContext);
+  const map = useMap();
+
+  const onEachFeature = (feature, layer) => {
+    if (feature.properties && feature.properties.site) {
+      layer.bindPopup(feature.properties.site);
+    }
+  };
 
   const geojsonStyle = {
     color: 'red',
     weight: 2,
-    opacity: 0.6
+    opacity: 0.6,
+    fillColor: 'red',
+    fillOpacity: 0.2
   };
 
-  return <GeoJSON data={geojsonData} style={geojsonStyle} />;
+  console.log('OverlayComponent received GeoJSON data:', geojsonData); // Debug log
+
+  if (geojsonData) {
+    // Fit the map bounds to the GeoJSON layer bounds
+    const geoJsonLayer = L.geoJson(geojsonData);
+    map.fitBounds(geoJsonLayer.getBounds());
+  }
+
+  return geojsonData ? (
+    <GeoJSON data={geojsonData} style={geojsonStyle} onEachFeature={onEachFeature} />
+  ) : null;
 };
 
 export default OverlayComponent;
---
+++ warning_data.py
@@ -0,0 +1,58 @@
+from awips.dataaccess import DataAccessLayer
+from datetime import datetime, timedelta
+from dynamicserialize.dstypes.com.raytheon.uf.common.time import TimeRange
+import json
+from shapely.geometry import mapping
+
+
+DataAccessLayer.changeEDEXHost("edex-cloud.unidata.ucar.edu")
+request = DataAccessLayer.newDataRequest()
+request.setDatatype("warning")
+params = ["phensig", "sig"]
+request.setParameters(*(params))
+
+
+# Get records from the last 12 hours
+lastHourDateTime = datetime.utcnow() - timedelta(hours = 12)
+start = lastHourDateTime.strftime('%Y-%m-%d %H:%M:%S')
+end = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+
+beginRange = datetime.strptime( start , "%Y-%m-%d %H:%M:%S")
+endRange = datetime.strptime( end , "%Y-%m-%d %H:%M:%S")
+timerange = TimeRange(beginRange, endRange)
+
+
+
+request = DataAccessLayer.newDataRequest()
+request.setDatatype("warning")
+request.setParameters('phensig')
+times = DataAccessLayer.getAvailableTimes(request)
+response = DataAccessLayer.getGeometryData(request, times[-50:-1])
+
+
+geojson_wdata = {
+    "type": "FeatureCollection",
+    "features": []
+}
+
+for ob in response:
+    poly = ob.getGeometry()
+    site = ob.getLocationName()
+    pd = ob.getDataTime().getValidPeriod()
+    ref = ob.getDataTime().getRefTime()
+
+    feature = {
+        "type": "Feature",
+        "geometry": mapping(poly),
+        "properties": {
+            "site": site,
+            "valid_period": str(pd),
+            "reference_time": str(ref)
+        }
+    }
+    
+    geojson_wdata["features"].append(feature)
+
+# Save the geojson_data to a file
+with open("/users/ahmedmosaad/documents/skyedge/MY-MAP/warnings.geojson", "w") as f:
+    json.dump(geojson_wdata, f)

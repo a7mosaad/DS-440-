ba88b898fc75b44c22443d3599f333ce435a5a40
a7mosaad <70121585+a7mosaad@users.noreply.github.com>
2024-07-18 10:54:52 -0400

    Update GeoDataContext.js

--- src/GeoDataContext.js
+++ src/GeoDataContext.js
@@ -1,9 +1,28 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect, useContext } from 'react';
 
-export const GeoDataContext = createContext();
+const GeoDataContext = createContext();
+
+export const useGeoData = () => {
+  return useContext(GeoDataContext);
+};
 
 export const GeoDataProvider = ({ children }) => {
   const [geojsonData, setGeojsonData] = useState(null);
+
+  useEffect(() => {
+    fetch('/warnings.geojson')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('GeoJSON data:', data);
+        setGeojsonData(data);
+      })
+      .catch(error => console.error('Error fetching GeoJSON data:', error));
+  }, []);
 
   return (
     <GeoDataContext.Provider value={{ geojsonData, setGeojsonData }}>
